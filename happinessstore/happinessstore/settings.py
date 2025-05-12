1da137c8c3b9c668b3f71c2e6661eb11ecf27c24
sourav <souravvikas10@gmail.com>
2025-05-12 23:57:01 +0530

    added allowed host

--- happinessstore/happinessstore/settings.py
+++ happinessstore/happinessstore/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config 
+from decouple import config
+ 
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = [config("ALLOWED_HOSTS", default="happiness-store.onrender.com")]
-
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="happiness-store.onrender.com").split(',')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
