36fc04283e0493e78833004f64c268c56895a768
sourav <souravvikas10@gmail.com>
2025-05-13 00:10:08 +0530

    collected static files

--- happinessstore/happinessstore/settings.py
+++ happinessstore/happinessstore/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'happinessstore.wsgi.application'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
